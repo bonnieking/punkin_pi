d423e9bce10b80b14d35f99567155eee094c8f07
Bonnie King <bonniek@kay.(none)>
2012-10-14 19:29:30 -0500

    default to first file in the listdir if no file specified in the url

--- player.py
+++ player.py
@@ -11,45 +11,31 @@ def config(config):
 
 player_html = os.path.join(os.path.dirname(__file__), 'player.html')
 
-@bobo.query('/')
-def index():
-    return """<html><head><title>Bobo Wiki</title></head><body>
-    SCARY MUSIC 
-    <hr />
-    %(docs)s
-    </body></html>
-    """ % dict(
-        docs='<br />'.join('<a href="%s?button=start">%s</a>' % (name, name)
-                           for name in sorted(os.listdir(media_dir)))
-        )
-
 @bobo.query('/:name')
-def toggle(bobo_request, name, button):
+def toggle(name=None, button='start'):
     if button not in ['start', 'stop']:
         return '500'
-    foo = "BAR"
-    path = os.path.join(media_dir, name)
 
-    if button == 'start':
-        foo = 'we are playing, so we will stop'
-        if os.path.exists(path):
-            audioplayer.start(path)
-            button = 'stop'
-        else:
-            return "No Such File %s" % (name)
+    files='<br />'.join('<a href="%s?button=start">%s</a>' % (name, name)
+                       for name in sorted(os.listdir(media_dir)))
 
-    elif button == 'stop':
-        foo = 'we are stopped, so we will play'
-        audioplayer.stop()
-        button = 'start'
+    if not name:
+        name = sorted(os.listdir(media_dir))[0]
+        
+    else:    
+        path = os.path.join(media_dir, name)
+        if button == 'start':
+            foo = 'we are playing, so we will stop'
+            if os.path.exists(path):
+                audioplayer.start(path)
+                button = 'stop'
+            else:
+                return "No Such File %s" % (name)
+
+        elif button == 'stop':
+            foo = 'we are stopped, so we will play'
+            audioplayer.stop()
+            button = 'start'
     return open(player_html).read() % dict(name=name, 
-        button=button, b=bobo_request, foo=foo)
+        button=button, files=files)
 
-
-
-#@bobo.query('/:name')
-#def player(name, action='start'):
-#    foo = 'this is a get'
-#    button = 'start'
-#    return open(player_html).read() % dict(name=name, 
-#        button=button, foo=foo, b='get')
