81e26b937d59c9c4117df2ea9711cbcead073d95
root <root@raspberrypi.(none)>
2012-10-16 21:05:26 -0500

    doesnt work yet

--- dbus_blinky/blinky_service.py
+++ dbus_blinky/blinky_service.py
@@ -13,7 +13,7 @@ from dbus import glib
 glib.init_threads()
 
 class Blinker():
-    def __init__(self, mode, pin):
+    def __init__(self, pin, mode):
        self.pin = pin
        self.mode = mode
        GPIO.setmode(self.mode)
@@ -26,6 +26,8 @@ class Blinker():
         GPIO.output(self.pin, GPIO.LOW)
 
     def blink(self, delay):
+        # can't send floats over dbus?
+        delay = float(delay) / 1000
         self.on()
         time.sleep(delay)
         self.off()
@@ -39,80 +41,32 @@ class Blinker():
         self.cleanup()
         GPIO.setup(self.pin, GPIO.OUT)
 
-class BlinkRun(threading.Thread):
-    def __init__(self, delay, blinker):
-        self.blinker = blinker
-        self.delay = delay
-        self.stopme = False
-        self.terminated = False
-
-    def changedelay(self, units, increment=0.01):
-        # increase or decrease delay. Default increment 0.1secs
-        if self.delay + (units * increment) < 0:
-            self.delay = 0
-        else:
-            self.delay = self.delay + (units * increment)
-            print "new delay is %s" %self.delay
-
-    def delayup(self, increment=0.01):
-        self.delay = self.delay + abs(increment)
-        print "new delay is %s" %self.delay
-
-    def delaydown(self, increment=0.01):
-        if self.delay - abs(increment) < 0:
-            self.delay = 0
-        else:
-            self.delay = self.delay - abs(increment)
-        print "new delay is %s" %self.delay
-
-    def start(self):
-        self.thread = threading.Thread(None, self.run, None, (), {})
-        self.thread.start()
-
-    def run(self):
-        while self.stopme == False:
-            self.blinker.blink(self.delay)
-        self.terminated = True
-        self.blinker.cleanup()
-
-    def stop(self):
-        self.stopme = True
-        while self.terminated == False:
-            time.sleep(0.01)
-        self.blinker.reset()
-
-
 class BlinkyDBUSService(dbus.service.Object):
-    def __init__(self):
-        #self.blinker = Blinker(GPIO.BCM, self.pin)
+    def __init__(self, pin, mode):
+        self.pin = pin
+        self.blinker = Blinker(pin, GPIO.BCM)
         bus_name = dbus.service.BusName('com.bonnielking.BlinkyPi', bus=dbus.SystemBus())
-        dbus.service.Object.__init__(self, bus_name, '/com/bonnielking/BlinkyPi')
-        self.blinking = False
-        self.b = None
+        dbus.service.Object.__init__(self, bus_name, '/com/bonnielking/BlinkyPi'+str(self.pin))
 
     @dbus.service.method('com.bonnielking.BlinkyPi')
-    def start(self, pin):
-        if self.b is not None:
-            self.b.stop()
-            print "stopping current blinky"
-        print "gonna blink!"
-        self.b = BlinkRun(0.1, Blinker(GPIO.BCM, pin))
-        self.b.start()
-        return "BLINK!"
+    def on(self):
+        print "on"
+        self.blinker.on()
 
     @dbus.service.method('com.bonnielking.BlinkyPi')
-    def stop(self):
-        print "stop your blinkin!"
-        if self.b is not None:
-            self.b.stop()
-            return "STOP!"
-        else:
-            return "no thread"
-            # pass 
+    def off(self):
+        print "off"
+        self.blinker.off()
+
+    @dbus.service.method('com.bonnielking.BlinkyPi')
+    def blink(self, delay):
+        print "blink"
+        self.blinker.blink(delay)   
 
 DBusGMainLoop(set_as_default=True)
 delay = 1
-myservice = BlinkyDBUSService()
+myservice = BlinkyDBUSService(17, GPIO.BCM)
+myservice = BlinkyDBUSService(18, GPIO.BCM)
 loop = gobject.MainLoop()
 loop.run()
 
