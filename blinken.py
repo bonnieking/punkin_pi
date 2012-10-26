d2ee5a8521073262aa9bf622c98d999a216812ca
Drew Fustini <github@pdp7.com>
2012-10-26 01:58:40 -0500

    removed hardcoded 2nd pin from Blinker and now pass 2nd pin as argument to Blinker constructor

--- blinken.py
+++ blinken.py
@@ -9,20 +9,21 @@ mode = GPIO.BCM
 
 class Blinker():
     #TODO: modify to take 2nd pin argument in order to properly toggle between 2 pins without any hardcoding
-    def __init__(self, mode, pin):
-       self.pin = pin
+    def __init__(self, mode, pin1, pin2):
+       self.pin1 = pin1
+       self.pin2 = pin2
        self.mode = mode
        GPIO.setmode(self.mode)
-       GPIO.setup(self.pin, GPIO.OUT)
-       GPIO.setup(18, GPIO.OUT)
+       GPIO.setup(self.pin1, GPIO.OUT)
+       GPIO.setup(self.pin2, GPIO.OUT)
 
     def on(self):
-        GPIO.output(self.pin, GPIO.HIGH)
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(self.pin1, GPIO.HIGH)
+        GPIO.output(self.pin2, GPIO.LOW)
  
     def off(self):
-        GPIO.output(self.pin, GPIO.LOW)
-        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(self.pin1, GPIO.LOW)
+        GPIO.output(self.pin2, GPIO.HIGH)
 
     def blink(self, delay):
         self.on()
@@ -31,16 +32,18 @@ class Blinker():
         time.sleep(delay)
 
     def cleanup(self):
-        # this is the default state of the pin
-        GPIO.setup(self.pin, GPIO.IN, GPIO.PUD_OFF)
+        # this is the default state of the pins
+        GPIO.setup(self.pin1, GPIO.IN, GPIO.PUD_OFF)
+        GPIO.setup(self.pin2, GPIO.IN, GPIO.PUD_OFF)
 
     def reset(self):
         self.cleanup()
-        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.setup(self.pin1, GPIO.OUT)
+        GPIO.setup(self.pin2, GPIO.OUT)
 
 class BlinkRun(threading.Thread):
     def __init__(self, delay):
-        self.blinker = Blinker(GPIO.BCM, 17)
+        self.blinker = Blinker(GPIO.BCM, 17, 18)
         self.delay = delay 
         self.stoprequest = threading.Event()
         self.stoprequest.clear()         
@@ -72,7 +75,6 @@ class BlinkRun(threading.Thread):
     def run(self):
         while not self.stoprequest.isSet():
             self.blinker.blink(self.delay)
-   #         self.stoprequest.wait()
         self.blinker.cleanup()
         pass
 
